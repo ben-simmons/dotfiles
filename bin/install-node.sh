89f095acf782797f38bfd976a42ddd201da34fd3
Ben Simmons <simmons.a.ben@gmail.com>
2021-08-16 19:30:34 -0700

    Add install node script

---
+++ bin/install-node.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# Source the nvm() shell command
+source ${NVM_DIR}/nvm.sh
+
+# Set the default to node v14
+nvm install 14
+nvm alias default 14
+nvm use default
+
+# Install global npm packages
+npm install -g cdk
