01d62bb281973e1b07baaedca89598bc958f49be
Ben Simmons <simmons.a.ben@gmail.com>
2021-11-14 16:03:49 -0800

    Comment out install-sublime.sh

--- bin/install.sh
+++ bin/install.sh
@@ -4,7 +4,7 @@ CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 source ${CWD}/install-brew.sh
 source ${CMD}/install-node.sh
-source ${CWD}/install-sublime.sh
+#source ${CWD}/install-sublime.sh
 source ${CWD}/install-symlink.sh
 
 chsh -s $(brew --prefix)/bin/zsh
