85ed9ff93384a5822176e702978d25ee638304a0
Ben Simmons <simmons.a.ben@gmail.com>
2025-02-23 15:07:43 -0800

    Add docker

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -97,6 +97,7 @@ brew install ollama
 
 # Casks
 #brew install --cask cursor
+brew install --cask docker
 #brew install --cask google-chrome
 #brew install --cask iterm2
 #brew install --cask spotify
